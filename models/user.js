23700ba24a0240bb692c9cfd485dbdb9650d9743
Alex Karolis <alexi2@protonmail.com>
2016-07-17 19:19:13 +1000

    add user and password db update, add bcrypt hash password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our user model
 // Ensure email is both unique and is saved in lower case
@@ -7,6 +8,23 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On Save Hook, encrypt password
+
+userSchema.pre('save', (next) => {
+  const user = this;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
+
+      user.password = hash;
+      next();
+    })
+  })
 });
 
 // Create the model class
